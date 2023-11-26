64a8d96f25dbfceb94e02850ea4b15d305cd218e
Asilvazavala <asilvazavala@gmail.com>
2023-11-26 12:43:05 -0600

    added entrega

---
+++ app/entrega/page.tsx
@@ -0,0 +1,12 @@
+import { Location } from "@/components/Location";
+
+const Entrega = () => {
+  return (
+    <section className='bg-Secondary dark:bg-DarkSecondary w-full px-4 md:pl-36 pt-16 md:pt-8'>
+      <h1 className="text-3xl text-Accent font-bold mb-4">Entrega</h1>
+      <Location />
+    </section>
+  )
+};
+
+export default Entrega;
