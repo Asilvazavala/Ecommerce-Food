b8d9f9b92929cc113709c09835daab337c503a89
Asilvazavala <asilvazavala@gmail.com>
2023-11-26 13:18:21 -0600

    modified entrega section

--- app/entrega/page.tsx
+++ app/entrega/page.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import { Location } from "@/components/Location";
 
 const Deliver = () => {
   return (
     <section className='bg-Secondary dark:bg-DarkSecondary w-full px-4 md:pl-36 pt-16 md:pt-8'>
       <h1 className="text-3xl text-Accent font-bold mb-4">Entrega</h1>
-      {/* <Location /> */}
+      <Location />
     </section>
   )
 };
